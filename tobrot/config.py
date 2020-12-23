6a891e9ac9b855808aac113397060e64084b0ce5
insaneman22 <76441053+insaneman22@users.noreply.github.com>
2020-12-23 23:12:19 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -2,9 +2,9 @@ from tobrot.sample_config import Config
 #Fill your all data, read readme for reference
 
 class Config(Config):
-	TG_BOT_TOKEN= "1479911746:AAGsQvo-39r_iGXal_9-bIYm13jOMPv_b9Y"
+	TG_BOT_TOKEN= "1418865344:AAHB0FQJ9u5rX3XS_nl-fmpX2mpzLz-EqFI"
 	APP_ID = 2624913
 	API_HASH = "a204e6332a52ea40033b8215421bc81d"
-	OWNER_ID = 1485920883 #ID of bot owner
+	OWNER_ID = 857877300 #ID of bot owner
 	AUTH_CHANNEL = [-1001151086861]
 	
